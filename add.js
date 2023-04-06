813add6483276e86d5582cfb1987872bd94d120e
thevaidik <106925209+thevaidik@users.noreply.github.com>
2023-04-07 00:34:04 +0530

    { first comment}}

---
+++ add.js
@@ -0,0 +1,11 @@
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+let sum = num1 + num2;
+sum = sum + num1;
+
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+console.log(sum);
