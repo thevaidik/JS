d92df74ebb1a4f62aab0c1799b61d6bfff464fb9
thevaidik <106925209+thevaidik@users.noreply.github.com>
2023-04-07 17:19:32 +0530

    4th commit, adding another comment

--- add.js
+++ add.js
@@ -5,7 +5,8 @@ const num2 = 3;
 let sum = num1 + num2;
 sum = sum + num1;
 
-
 // display the sum
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
 console.log(sum);
+
+// here goes the 4th commit, learning in process
