9e1f65ed94a648be7025de46424c9aba20db9c57
thevaidik <106925209+thevaidik@users.noreply.github.com>
2023-06-29 01:38:24 +0530

    3 requirements done and comments added

--- Slot machine/slotmachine.js
+++ Slot machine/slotmachine.js
@@ -8,17 +8,17 @@
 //function deposit() {
    // return 1
 //}
-const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")(); //declaring prompt-sync is required
 
 
 const deposit = () => {
-    while (true) {
+    while (true) {  //while loop to keep program running if criteria is not met
 
 
         const depositamount = prompt("enter a deposit amount");
-        const numberdepositamount = Number(depositamount);
+        const numberdepositamount = Number(depositamount); //Number method to convert string in int
 
-        if (isNaN(numberdepositamount) || numberdepositamount <= 0) {
+        if (isNaN(numberdepositamount) || numberdepositamount <= 0) {   //isNan to check if its number
             console.log("invalid deposit amount, Try again")
         }
         else {
@@ -27,8 +27,42 @@ const deposit = () => {
     }
 }
 
-const depositamount = deposit();
-console.log(depositamount);
+const getnumberoflines = () => {
+    while(true){
+        const lines =prompt("enter no of lines to bet on");
+        const numberoflines = Number(lines);
+
+        if(isNaN(numberoflines) || numberoflines <=0 || numberoflines >=3) {
+            console.log("invalid number of lines , try again");
+        }
+        else {
+            return numberoflines;
+        }
+    }
+
+}
+
+const getbet = (balance) => {
+    while (true){
+        const bet = prompt("enter the total bet");
+        const numberbet = Number(bet);
+
+        if(isNaN(numberbet) || numberbet <= 0 || numberbet>balance) {
+            console.log("invalid bet");
+        }
+        else {
+            return numberbet;
+        }
+    }
+}
+
+let balance = deposit();
+const numberoflines = getnumberoflines();
+const bet = getbet(balance)
+
+
+
+
 
 
 
