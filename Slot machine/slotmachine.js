980b88c0bafcceeb537c35cd589d1c1c0bf9381b
thevaidik <106925209+thevaidik@users.noreply.github.com>
2023-07-04 20:09:39 +0530

    comments

--- Slot machine/slotmachine.js
+++ Slot machine/slotmachine.js
@@ -28,7 +28,7 @@ const SYMBOL_VALUES ={
 }
 
 
-const deposit = () => {
+const deposit = () => {  //deposit amount
     while (true) {  //while loop to keep program running if criteria is not met
 
 
@@ -44,7 +44,7 @@ const deposit = () => {
     }
 }
 
-const getnumberoflines = () => {
+const getnumberoflines = () => {  //lines to bet on
     while(true){
         const lines =prompt("enter no of lines to bet on (1-3)");
         const numberoflines = Number(lines);
@@ -58,7 +58,7 @@ const getnumberoflines = () => {
     }
 
 }
-const getbet = (numberdepositamount, numberoflines) => {
+const getbet = (numberdepositamount, numberoflines) => {  //getting bet per line
     while (true){
         const bet = prompt("enter the bet per line ");
         const numberbet = Number(bet);
@@ -73,12 +73,14 @@ const getbet = (numberdepositamount, numberoflines) => {
 }
 
 const spin = () => {
-    const symbols = []; //creating an empty array
+    const symbols = []; //creating an array to fit all values on wheel.
     for (const [symbol, count] of Object.entries(SYMBOLS_COUNT) ){  //using Object.entries method and
         //and assigning symbols to variables symbols and count
         for(let i=0 ; i< count ; i++ ){
             symbols.push(symbol);  //push method do add element "symbol" in array symbols.
-        } //function is adding wheel number to array
+        } //function will go to symbol count(using object.entries loop) and then using another for loop ,
+        // add how many times it is there in array.
+
 
 
     }
@@ -86,15 +88,15 @@ const spin = () => {
     for (let i=0; i<COLS; i++){  //pushing arr into arr equal to no of cols, to make a matrix(wheel)
         reels.push([]);
 
-        const reelsSymbols = [...symbols];
+        const reelsSymbols = [...symbols]; //copy array to avoid editing original array
         for (let j= 0; j<ROWS;j++ ){
-            const randomIndex = Math.floor(Math.random() * reelsSymbols.length);
-            const selectedSymbol = reelsSymbols[randomIndex];
-            reels[i].push(selectedSymbol);
-            reelsSymbols.splice(randomIndex, 1);
+            const randomIndex = Math.floor(Math.random() * reelsSymbols.length); //generating a random index
+            const selectedSymbol = reelsSymbols[randomIndex]; //selecting the random symbol
+            reels[i].push(selectedSymbol); //random symbol is pushed into reel array
+            reelsSymbols.splice(randomIndex, 1); //deleting selected random symbol as it cant be reused
         }
     }
-    console.log(reels);
+    console.log(symbols);
 };
 
 //check if only one vaiable to assigned to function while funtions returns many values , will it cause an error
--- add.js
+++ add.js
@@ -2,12 +2,15 @@ const num1 = 5;
 const num2 = 3;
 
 // add two numbers
-let sum = num1 + num2;
-sum = sum + num1;
+const add = () => {
+    const finaladd = num1+num2;
+    const final_divide = num1/num2;
+    return {finaladd,final_divide};
 
-// display the sum
-console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
-console.log(sum);
+
+}
+
+console.log(add());
 
 // here goes the 4th commit, learning in process
 //on the 5th commit trying to add a new line
