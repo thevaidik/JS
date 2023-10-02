cab4b127de66bb3b491ec8430545805fe836f0c5
thevaidik <106925209+thevaidik@users.noreply.github.com>
2023-10-03 02:04:39 +0530

    prettier applied

--- Slot machine/slotmachine.js
+++ Slot machine/slotmachine.js
@@ -6,7 +6,7 @@
 // 6- give the user their winnings
 // 7- play again
 //function deposit() {
-   // return 1
+// return 1
 //}
 const prompt = require("prompt-sync")(); //declaring prompt-sync is required
 
@@ -14,35 +14,35 @@ const ROWS = 3;
 const COLS = 3;
 
 const SYMBOLS_COUNT = {
-    A: 2,
-    B: 4,
-    C: 6,
-    D: 8
-}
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
 
-const SYMBOL_VALUES ={
-    A: 5,
-    B: 4,
-    C: 3,
-     D : 2
-}
+const SYMBOL_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
 
+const deposit = () => {
+  //deposit amount
+  while (true) {
+    //while loop to keep program running if criteria is not met
 
-const deposit = () => {  //deposit amount
-    while (true) {  //while loop to keep program running if criteria is not met
+    const depositamount = prompt("enter a deposit amount");
+    const numberdepositamount = Number(depositamount); //Number method to convert string in int
 
-
-        const depositamount = prompt("enter a deposit amount");
-        const numberdepositamount = Number(depositamount); //Number method to convert string in int
-
-        if (isNaN(numberdepositamount) || numberdepositamount <= 0) {   //isNan to check if its number
-            console.log("invalid deposit amount, Try again")
-        }
-        else {
-            return numberdepositamount;
-        }
+    if (isNaN(numberdepositamount) || numberdepositamount <= 0) {
+      //isNan to check if its number
+      console.log("invalid deposit amount, Try again");
+    } else {
+      return numberdepositamount;
     }
-}
+  }
+};
 
 const getnumberoflines = () => {
   //lines to bet on
@@ -50,133 +50,107 @@ const getnumberoflines = () => {
     const lines = prompt("enter no of lines to bet on (1-3)");
     const numberoflines = Number(lines);
 
-        if(isNaN(numberoflines) || numberoflines <=0 || numberoflines >3) {
-            console.log("invalid number of lines , try again");
-        }
-        else {
-            return numberoflines;
-        }
+    if (isNaN(numberoflines) || numberoflines <= 0 || numberoflines > 3) {
+      console.log("invalid number of lines , try again");
+    } else {
+      return numberoflines;
     }
+  }
+};
+const getbet = (numberdepositamount, numberoflines) => {
+  //getting bet per line
+  while (true) {
+    const bet = prompt("enter the bet per line ");
+    const numberbet = Number(bet);
 
-}
-const getbet = (numberdepositamount, numberoflines) => {  //getting bet per line
-    while (true){
-        const bet = prompt("enter the bet per line ");
-        const numberbet = Number(bet);
-
-        if(isNaN(numberbet) || numberbet <= 0 || numberbet > numberdepositamount / numberoflines) {
-            console.log("invalid bet");
-        }
-        else {
-            return numberbet;
-        }
+    if (
+      isNaN(numberbet) ||
+      numberbet <= 0 ||
+      numberbet > numberdepositamount / numberoflines
+    ) {
+      console.log("invalid bet");
+    } else {
+      return numberbet;
     }
-}
-
-const spin = () => {
-    const symbols = []; //creating an array to fit all values on wheel.
-    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT) ){  //using Object.entries method and
-        //and assigning symbols to variables symbols and count
-        for(let i=0 ; i< count ; i++ ){
-            symbols.push(symbol);  //push method do add element "symbol" in array symbols.
-        } //function will go to symbol count(using object.entries loop) and then using another for loop ,
-        // add how many times it is there in array.
-
-
-
-    }
-    const reels = [];
-    for (let i=0; i<COLS; i++){  //pushing arr into arr equal to no of cols, to make a matrix(wheel)
-        reels.push([]);
-
-        const reelsSymbols = [...symbols]; //copy array to avoid editing original array
-        for (let j= 0; j<ROWS;j++ ){
-            const randomIndex = Math.floor(Math.random() * reelsSymbols.length); //generating a random index
-            const selectedSymbol = reelsSymbols[randomIndex]; //selecting the random symbol
-            reels[i].push(selectedSymbol); //random symbol is pushed into reel array
-            reelsSymbols.splice(randomIndex, 1); //deleting selected random symbol as it cant be reused
-        }
-    }
-    return reels;
-
+  }
 };
 
-const transpose= (reels) =>{
-    const rows = [];
+const spin = () => {
+  const symbols = []; //creating an array to fit all values on wheel.
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    //using Object.entries method and
+    //and assigning symbols to variables symbols and count
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol); //push method do add element "symbol" in array symbols.
+    } //function will go to symbol count(using object.entries loop) and then using another for loop ,
+    // add how many times it is there in array.
+  }
+  const reels = [];
+  for (let i = 0; i < COLS; i++) {
+    //pushing arr into arr equal to no of cols, to make a matrix(wheel)
+    reels.push([]);
 
-    for(let i = 0; i < ROWS; i++){
-        rows.push([]);
-        for(let j= 0; j<COLS ; j++){
-            rows[i].push(reels[j][i]);
-        }
+    const reelsSymbols = [...symbols]; //copy array to avoid editing original array
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelsSymbols.length); //generating a random index
+      const selectedSymbol = reelsSymbols[randomIndex]; //selecting the random symbol
+      reels[i].push(selectedSymbol); //random symbol is pushed into reel array
+      reelsSymbols.splice(randomIndex, 1); //deleting selected random symbol as it cant be reused
     }
-    return rows;
+  }
+  return reels;
+};
 
+const transpose = (reels) => {
+  const rows = [];
 
-}
-
-
-const printrows = (rows) =>
-{
-    for(const row of rows) {
-        let rowstring = "";
-        for (const[i,symbol] of row.entries ()){
-            rowstring += symbol
-            if (i != row.length -1 ){
-                rowstring += "|"
-            }
-        }
-        console.log(rowstring);
-
-
+  for (let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for (let j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
     }
-}
+  }
+  return rows;
+};
 
-const getWinnings = (rows,bets,lines )=>
-{
-    let winnings = 0;
-
-    for(let row = 0; row < lines; row++){
-        const symbols = rows[row];
-        let allsame = true;
-
-
-        for(const symbol of symbols ){
-            if (symbol != symbols[0]) {
-                allsame = false;
-                break;
-            }
-        }
-        if (allsame){
-            winnings += bets * SYMBOL_VALUES[symbols[0]]
-        }
+const printrows = (rows) => {
+  for (const row of rows) {
+    let rowstring = "";
+    for (const [i, symbol] of row.entries()) {
+      rowstring += symbol;
+      if (i != row.length - 1) {
+        rowstring += "|";
+      }
     }
-    return winnings
+    console.log(rowstring);
+  }
+};
 
-}
+const getWinnings = (rows, bets, lines) => {
+  let winnings = 0;
 
+  for (let row = 0; row < lines; row++) {
+    const symbols = rows[row];
+    let allsame = true;
 
-
-
-
+    for (const symbol of symbols) {
+      if (symbol != symbols[0]) {
+        allsame = false;
+        break;
+      }
+    }
+    if (allsame) {
+      winnings += bets * SYMBOL_VALUES[symbols[0]];
+    }
+  }
+  return winnings;
+};
 
 let fnbalance = deposit();
 const fnnumberoflines = getnumberoflines();
-const fnbet = getbet(fnbalance,fnnumberoflines);
+const fnbet = getbet(fnbalance, fnnumberoflines);
 const reels = spin();
 const rows = transpose(reels);
 printrows(rows);
-const winnings = getWinnings(rows,fnbet,fnnumberoflines);
-console.log ("You won, $" + winnings.toString());
-
-
-
-
-
-
-
-
-
-
-
-
+const winnings = getWinnings(rows, fnbet, fnnumberoflines);
+console.log("You won, $" + winnings.toString());
